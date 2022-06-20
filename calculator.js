beb74185b61f75841f34180c03acc9df131e2553
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-06-20 14:51:21 -0400

    Added new code for BMI Calculator

--- calculator.js
+++ calculator.js
@@ -18,6 +18,19 @@ app.post("/", function(req, res) {
     res.send("The result of the calculation is " + result);
 });
 
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmicalculator", function(req, res) {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+    
+    res.send ("Your BMI is " + bmi);
+});
+
 app.listen(port, function() {
     console.log("Server started on port 3000");
 });
