c627e70fadc3e145ba4dadfba602efb005c757d4
lexus9425 <68475116+lexus9425@users.noreply.github.com>
2022-06-20 14:34:06 -0400

    Initial Upload

---
+++ calculator.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({extended:true}));
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
+});
+
+app.listen(port, function() {
+    console.log("Server started on port 3000");
+});
